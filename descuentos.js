aef9c77345239299356a1a075cbddee94963636e
diegocardona8 <dc.soft08@gmail.com>
2021-12-23 21:47:04 -0500

    Conexión formulario de porcentaje con funciones, conexión del resultado con HTML

--- descuentos.js
+++ descuentos.js
@@ -3,16 +3,25 @@ function calculateDiscountedPrice (price,discount){
 
     const percentageDiscountedPrice = 100 - discount;
     const priceDiscount = (price * percentageDiscountedPrice)/100;
+    
 
     return priceDiscount ; 
 
+    
 }
 
-// console.log({
+function onClickButtonPriceDiscount(){
 
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento,
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
 
-// }); 
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const priceWidthDiscount = calculateDiscountedPrice(priceValue,discountValue);
+
+    const resultP = document.getElementById("ResultP");
+
+    resultP.innerText = "El precio con descuento es $" + priceWidthDiscount;
+    
+}
