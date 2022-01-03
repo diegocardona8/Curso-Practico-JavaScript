9b2eaa2a5e89c208ef34aaf0abf1d27b276d02e0
diegocardona8 <dc.soft08@gmail.com>
2022-01-02 21:50:58 -0500

    Aplicación estructuración Hash Table en el descuento con cupones

--- descuentosCupones.js
+++ descuentosCupones.js
@@ -4,6 +4,7 @@ function calcularPrecioConDescuento (precio,descuento){
     const porcentajeDelPrecioConDescuento  = 100 - descuento;
     const precioConDescuento  = (precio * porcentajeDelPrecioConDescuento)/100;
     
+    return precioConDescuento;
 }
 
 function onClickButtonPriceDiscount(){
