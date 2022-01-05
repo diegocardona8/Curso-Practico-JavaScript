23f4b587fc1fe4c8be28041d9adf63b3068e9ca0
diegocardona8 <dc.soft08@gmail.com>
2022-01-05 17:14:21 -0500

    New concept , learned

---
+++ parametrosNombrados.js
@@ -0,0 +1,25 @@
+//Forma Tradicional 
+
+function crearmascota (color,peso,ciudad,natalidad){
+    
+   
+}
+
+crearmascota("amarillo",100,neiva);
+
+// With Parametros Nombrados 
+
+//Atraves de la sintaxis de Desestructuración 
+//Haciendo un contrato entre los argumentos a traves del nombre, veamos
+
+function crearfinca ({ubicacion,precio,dimension,material='metal',dueños}){
+
+
+}
+
+crearfinca({
+
+    ubicacion:"chucuni",
+    precio: 2000, 
+    dimension:2000, 
+    dueños: ["Ismael" , "Nena"]}); 
